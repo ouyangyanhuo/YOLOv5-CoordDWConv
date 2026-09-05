0a03d2ffefb818ba27419ab11d6b21d626f5d8a1
ouyangyanhuo <ouyangyanhuo@vip.qq.com>
2026-09-05 21:35:30 +0800

    更新 .gitignore 以排除 yolov5s.pt 和 yolov5n.pt 文件；修复 common.py 中的类型转换为 np.float32；在 loggers 中使用 packaging.version 替代 pkg_resources；改进 metrics.py 中的 AP 计算方法以支持 NumPy 2.0+

--- utils/loggers/__init__.py
+++ utils/loggers/__init__.py
@@ -6,7 +6,7 @@ import os
 import warnings
 from pathlib import Path
 
-import pkg_resources as pkg
+from packaging.version import Version
 import torch
 
 from utils.general import LOGGER, colorstr, cv2
@@ -31,7 +31,7 @@ try:
     import wandb
 
     assert hasattr(wandb, "__version__")  # verify package import not local dir
-    if pkg.parse_version(wandb.__version__) >= pkg.parse_version("0.12.2") and RANK in {0, -1}:
+    if Version(wandb.__version__) >= Version("0.12.2") and RANK in {0, -1}:
         try:
             wandb_login_success = wandb.login(timeout=30)
         except wandb.errors.UsageError:  # known non-TTY terminal issue
